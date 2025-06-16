7a9fdda43e025ed888fe595f4e0455277876f4aa
Muhammad-Nouman-Khan <nouman.younas.nyk@gmail.com>
2025-06-16 19:15:56 +0500

    Reinit

--- app/(auth)/layout.tsx
+++
@@ -1,8 +0,0 @@
-import React, { ReactNode } from "react";
-
-//whats reactnode ? : it is a type that represents any react element.
-const AuthLayout = ({ children }: { children: ReactNode }) => {
-  return <div className="auth-layout">{children}</div>;
-};
-
-export default AuthLayout;
--- app/(root)/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const page = () => {
-  return <div>HomePage</div>;
-};
-
-export default page;
