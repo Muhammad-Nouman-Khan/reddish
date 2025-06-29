e892ba9829d7516d2928575e4a8e4063276a85c5
Muhammad-Nouman-Khan <nouman.younas.nyk@gmail.com>
2025-06-29 18:19:23 +0500

    Update global styles and layout theme; remove unused CSS variables and switch layout theme from 'synthwave' to 'forest' for a refreshed design.

--- src/app/layout.js
+++ src/app/layout.js
@@ -19,7 +19,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html data-theme="synthwave" lang="en">
+    <html data-theme="forest" lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         suppressHydrationWarning
