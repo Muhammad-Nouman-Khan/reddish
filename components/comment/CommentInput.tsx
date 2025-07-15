af261b7832aff642fd79f08c94c8d0741f7b879a
Muhammad-Nouman-Khan <nouman.younas.nyk@gmail.com>
2025-07-15 22:40:09 +0500

    comment input

--- components/comment/CommentInput.tsx
+++ components/comment/CommentInput.tsx
@@ -1,4 +1,9 @@
 "use client";
+
+import { useUser } from "@clerk/nextjs";
+import { useRouter } from "next/navigation";
+import { useState, useTransition } from "react";
+
 const CommentInput = ({
   postId,
   parentCommentId,
@@ -6,6 +11,10 @@ const CommentInput = ({
   postId: string;
   parentCommentId?: string;
 }) => {
+  const [content, setContent] = useState("");
+  const [isPending, startTransition] = useTransition();
+  const router = useRouter();
+  const { user } = useUser();
   return <div>CommentInput</div>;
 };
 
