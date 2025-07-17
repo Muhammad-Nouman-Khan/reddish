6ad73ac4709a40982920650a2df37a4cfc50d35a
Muhammad-Nouman-Khan <nouman.younas.nyk@gmail.com>
2025-07-17 22:01:49 +0500

    Enhance Comment component to accept props for postId, comment, and userId

--- components/comment/Comment.tsx
+++ components/comment/Comment.tsx
@@ -1,6 +1,15 @@
+import { GetPostCommentsQueryResult } from "@/sanity.types";
 import React from "react";
 
-const Comment = () => {
+const Comment = ({
+  postId,
+  comment,
+  userId,
+}: {
+  postId: string;
+  comment: GetPostCommentsQueryResult[number];
+  userId: string | null;
+}) => {
   return <div>Comment</div>;
 };
 
