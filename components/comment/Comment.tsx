2eb4ce36556d1c1be8b860de84335ef8afccffc1
Muhammad-Nouman-Khan <nouman.younas.nyk@gmail.com>
2025-07-19 18:32:28 +0500

    Enhance Comment component to include user profile image display with fallback icon

--- components/comment/Comment.tsx
+++ components/comment/Comment.tsx
@@ -3,6 +3,8 @@ import {
   GetPostCommentsQueryResult,
 } from "@/sanity.types";
 import { getCommentReplies } from "@/sanity/lib/comment/getCommentReplies";
+import { UserCircle } from "lucide-react";
+import Image from "next/image";
 import React from "react";
 
 const Comment = async ({
@@ -19,8 +21,23 @@ const Comment = async ({
   const replies = await getCommentReplies(comment._id, userId);
   const userVoteStatus = comment.votes.voteStatus;
   return (
-    <article>
-      <div></div>
+    <article className="py-5 border-b border-gray-100 last:border-0">
+      <div className="flex gap-4">
+        {/* PostVoteButtons */}
+        <div className="flex-1 space-y-2">
+          <div className="flex flex-wrap items-center gap-2">
+            {comment.author?.imageUrl ? (
+              <div className="flex-shrink-0">
+                <Image src={comment.author.imageUrl} alt={`${comment.author.username}'s profile`} className="w-10 h-10 rounded-full object-cover" width={40} height={40} />
+              </div>
+            ):(
+              <div className="flex-shrink-0">
+                <UserCircle className="w-10 h-10 text-gray-300" />
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
     </article>
   );
 };
