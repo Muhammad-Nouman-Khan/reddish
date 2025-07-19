388bc577d28a97acb28714e7faf8e458f177a04d
Muhammad-Nouman-Khan <nouman.younas.nyk@gmail.com>
2025-07-19 19:40:47 +0500

    Add voting buttons to PostVoteButtons component with upvote and downvote functionality

--- components/post/PostVoteButtons.tsx
+++ components/post/PostVoteButtons.tsx
@@ -4,6 +4,7 @@ import {
   GetUserPostVoteStatusQueryResult,
 } from "@/sanity.types";
 import { useUser } from "@clerk/nextjs";
+import { ArrowDown, ArrowUp } from "lucide-react";
 import { useState, useTransition } from "react";
 
 const PostVoteButtons = ({
@@ -25,9 +26,35 @@ const PostVoteButtons = ({
   );
   const [isPending, startTransition] = useTransition();
 
+  const handleUpVote = () => {};
+
+  const handleDownVote = () => {};
+
   return (
     <div className="flex flex-col items-center bg-gray-50 p-2 rounded-l-md">
-      PostVoteButtons
+      <button
+        disabled={!isSignedIn || isPending || !user}
+        onClick={handleUpVote}
+        className={`p-2 rounded disabled:opacity-50 disabled:cursor-not-allowed ${optimisticVote === "upvote" ? "bg-orange-100" : "hover:bg-gray-100"}
+        ${isPending && "opacity-50 cursor-not-allowed"}`}
+      >
+        <ArrowUp
+          className={`w-5 h-5 ${optimisticVote === "upvote" ? "text-orange-500 font-bold" : "text-gray-400 hover:text-orange-500"}`}
+        />
+      </button>
+      <span className="text-sm font-medium text-gray-900">
+        {optimisticScore}
+      </span>
+      <button
+        disabled={!isSignedIn || isPending || !user}
+        onClick={handleDownVote}
+        className={`p-2 rounded disabled:opacity-50 disabled:cursor-not-allowed ${optimisticVote === "downvote" ? "bg-blue-100" : "hover:bg-gray-100"}
+        ${isPending && "opacity-50 cursor-not-allowed"}`}
+      >
+        <ArrowDown
+          className={`w-5 h-5 ${optimisticVote === "downvote" ? "text-blue-500 font-bold" : "text-gray-400 hover:text-blue-500"}`}
+        />
+      </button>
     </div>
   );
 };
